575d37bcca7a9814d1edafb1fa69c6486fc385d0
Anna <117213428+Anna05a@users.noreply.github.com>
2024-02-28 12:51:23 +0200

    Практична робота №1

---
+++ pj.py
@@ -0,0 +1,10 @@
+def calculate_formula(x, a, b):
+    y = x**4 - x**2 - b/x - b * x**3 - x - a/x - a
+    return y
+
+a = -10
+b = -20
+x = -30
+
+y = calculate_formula(x, a, b)
+print("a =", a, ", b =", b, ", x =", x, ", y =", y)
